74bd1c6e375fefadbe958c791da29740a815c00c
Ignacio Arron <ignacioarron251@gmail.com>
2022-08-04 11:12:46 -0300

    main page progress, html mostly done, style/js pending

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+}
+
+function LoadItems() {
+    newGameButton = document.getElementById("new-game-button");
+    loadGameButton = document.getElementById("load-game-button");
+    contactButton = document.getElementById("contact-button");
+    gameName = document.getElementById("game-name");
+}
