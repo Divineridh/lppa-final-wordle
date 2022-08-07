ccb251b5481c75dd325908e317d67ffe0640fde9
Ignacio Arron <ignacioarron251@gmail.com>
2022-08-07 16:16:55 -0300

    button fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     loadItems();
-    newGameButton.onclick = () => {
-        location = "./wordle.html";
+    newGameButton.onclick = (e) => {
+        e.preventDefault();
+        showNewGameScreen();
     }
 }
 
@@ -11,8 +12,18 @@ function loadItems() {
     contactButton = document.getElementById("contact-button");
     gameName = document.getElementById("game-name");
     newGameModal = document.getElementById("newGameModal");
+    cancelGameButton = document.getElementById("new-game-cancel-button");
+    startGameButton = document.getElementById("new-game-begin-button");
 }
 
 function showNewGameScreen() {
     newGameModal.classList.add("modal-show");
+    cancelGameButton.onclick = () => {
+        newgameModal.classList.remove("modal-show");
+    }
+    window.onclick = function(e) {
+        if (e.target == newGameModal) {
+            newGameModal.classList.remove("modal-show");
+        }
+    }
 }
